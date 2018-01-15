2de1d08cd67354783260f6dded540855acb07272
Diaz Octavio <odiaz@lcs-mba3082.lcs.on.ca>
2018-01-15 13:50:00 -0500

    Guessing game first draft

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var guessedNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func GuessMade(_ sender: Any) {
+        
+        print("A guess was made")
+        print(guessedNumber.text)
+        
+    }
+    
 }
 
